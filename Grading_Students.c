a7be122995166a71c0cfb2a002c74671b78e6484
Nishant Aklecha <31594715+Naklecha@users.noreply.github.com>
2017-10-11 21:07:29 +0530

    Update Grading_Students.c

--- Grading_Students.c
+++ Grading_Students.c
@@ -6,27 +6,14 @@
 #include <limits.h>
 #include <stdbool.h>
 
-int* solve(int grades_size, int* grades, int *result_size){
-    // Complete this function
-}
-
-int main() {
+int main(){
     int n; 
-    scanf("%d", &n);
-    int *grades = malloc(sizeof(int) * n);
-    for(int grades_i = 0; grades_i < n; grades_i++){
-       scanf("%d",&grades[grades_i]);
+    scanf("%d",&n);
+    for(int a0 = 0; a0 < n; a0++){
+        int grade; 
+        scanf("%d",&grade);
+        if(grade>37 && (((grade/5)+1)*5)-grade<3) grade = ((grade/5)+1)*5;
+        printf("%d\n",grade);
     }
-    int result_size;
-    int* result = solve(n, grades, &result_size);
-    for(int result_i = 0; result_i < result_size; result_i++) {
-        if(result_i) {
-            printf("\n");
-        }
-        printf("%d", result[result_i]);
-    }
-    puts("");
-    
-
     return 0;
 }
