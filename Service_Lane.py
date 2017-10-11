6123426198e60a72d89ac4359e09e2d41e56ae93
Nishant Aklecha <31594715+Naklecha@users.noreply.github.com>
2017-10-11 21:25:17 +0530

    Create Service_Lane.py

---
+++ Service_Lane.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+import sys
+
+
+n,t = raw_input().strip().split(' ')
+n,t = [int(n),int(t)]
+width = map(int,raw_input().strip().split(' '))
+for a0 in xrange(t):
+    i,j = raw_input().strip().split(' ')
+    i,j = [int(i),int(j)]
+    m = 3
+    for k in range(i,j+1):
+        if m > width[k]:
+            m = width[k]
+    print m
