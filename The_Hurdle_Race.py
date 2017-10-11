76b5e97bf04bd7dcb1455c8b09756ac95015f147
Nishant Aklecha <31594715+Naklecha@users.noreply.github.com>
2017-10-11 21:52:05 +0530

    Create The_Hurdle_Race.py

---
+++ The_Hurdle_Race.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+import sys
+
+
+n,k = raw_input().strip().split(' ')
+n,k = [int(n),int(k)]
+height = map(int, raw_input().strip().split(' '))
+m = max(height)
+print(m-k if m>k else 0)
