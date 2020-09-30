3b1513e916767e90badc1bbb80c5dd0d9270272d
Ravitejavelamuri <39962669+Ravitejavelamuri@users.noreply.github.com>
2020-10-01 00:57:13 +0530

    Update Bigger_is_Greater.cpp

--- Bigger_is_Greater.cpp
+++ Bigger_is_Greater.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <algorithm>
 int main() {
-    int t;
-    std::cin>>t;
-    for(int i=0;i<t;i++) {
+    int k;
+    std::cin>>k;
+    for(int i=0;i<k;i++) {
         std::string input;
         std::cin>>input;
         if(std::next_permutation(input.begin(),input.end()))
             std::cout<<input<<std::endl;
         else
-            std::cout<<"no answer"<<std::endl;
+            std::cout<<"No Answer"<<std::endl;
     }
     return 0;
 }
